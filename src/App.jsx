dcd7479ab64dc295c6493ce78cb819da97c23c01
Thiago <thiagodelgado.dev@gmail.com>
2024-10-31 20:49:55 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./sections/Navbar"
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <h1 className="text-2xl text-white">
+        <Navbar />
+      </h1>
+    </main>
+  )
+}
+
+export default App
