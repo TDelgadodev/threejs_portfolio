1e9f4870e70500e964c8f7243ef63c2b5d6ffbb8
Thiago <thiagodelgado.dev@gmail.com>
2024-11-05 16:48:26 -0300

    Hero Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Hero from "./sections/Hero";
-import Navbar from "./sections/Navbar";
+import Hero from './sections/Hero.jsx';
+
+import Navbar from './sections/Navbar.jsx';
+
 
 const App = () => {
   return (
-    <main className="max-w-7xl mx-auto">
+    <main className="max-w-7xl mx-auto relative">
       <Navbar />
       <Hero />
     </main>
