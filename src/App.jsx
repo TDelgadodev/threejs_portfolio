0e5fa282420a69361706ac1d0931afb6efeea46a
Thiago <thiagodelgado.dev@gmail.com>
2024-11-05 19:46:26 -0300

    Bento Grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from './sections/About.jsx';
 import Hero from './sections/Hero.jsx';
 
 import Navbar from './sections/Navbar.jsx';
@@ -8,6 +9,7 @@ const App = () => {
     <main className="max-w-7xl mx-auto relative">
       <Navbar />
       <Hero />
+      <About />
     </main>
   );
 };
