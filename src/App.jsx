f32a334384ea77663b583bc740ae82692c8c9770
Thiago <thiagodelgado.dev@gmail.com>
2024-11-04 21:04:42 -0300

    setup to Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Navbar from "./sections/Navbar"
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
-      <h1 className="text-2xl text-white">
-        <Navbar />
-      </h1>
+      <Navbar />
+      <Hero />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
