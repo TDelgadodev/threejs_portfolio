80eb8ac23d9e5f42af3b59d0b3ceda468c667708
Thiago <thiagodelgado.dev@gmail.com>
2024-11-06 15:49:06 -0300

    replace test & image

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(" thiagodelgado.dev@gmail.com");
+    navigator.clipboard.writeText("thiagodelgado.dev@gmail.com");
     setHasCopied(true);
 
     setTimeout(() => {
@@ -27,11 +27,10 @@ const About = () => {
             />
 
             <div>
-              <p className="grid-headtext">Hi, I’m Adrian Hajdin</p>
+              <p className="grid-headtext">Hi, I’m Thiago Delgado</p>
               <p className="grid-subtext">
-                With 12 years of experience, I have honed my skills in both
-                frontend and backend dev, creating dynamic and responsive
-                websites.
+                I have developed my skills in frontend and backend development,
+                creating dynamic and responsive websites.
               </p>
             </div>
           </div>
@@ -49,7 +48,7 @@ const About = () => {
               <p className="grid-headtext">Tech Stack</p>
               <p className="grid-subtext">
                 I specialize in a variety of languages, frameworks, and tools
-                that allow me to build robust and scalable applications
+                that allow me to build robust and scalable applications.
               </p>
             </div>
           </div>
@@ -69,9 +68,9 @@ const About = () => {
                 bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
                 labelsData={[
                   {
-                    lat: 40,
-                    lng: -100,
-                    text: "Rjieka, Croatia",
+                    lat: -34.6037,
+                    lng: -58.3816,
+                    text: "Buenos Aires, Argentina",
                     color: "white",
                     size: 15,
                   },
@@ -83,7 +82,7 @@ const About = () => {
                 I’m very flexible with time zone communications & locations
               </p>
               <p className="grid-subtext">
-                I&apos;m based in Rjieka, Croatia and open to remote work
+                I’m based in Buenos Aires, Argentina, and open to remote work
                 worldwide.
               </p>
               <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
@@ -104,7 +103,7 @@ const About = () => {
               <p className="grid-subtext">
                 I love solving problems and building things through code.
                 Programming isn&apos;t just my profession—it&apos;s my passion.
-                I enjoy exploring new technologies, and enhancing my skills.
+                I enjoy exploring new technologies and enhancing my skills.
               </p>
             </div>
           </div>
